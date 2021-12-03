06bc174eebf156d6f7c33fddc63f6ad309e75794
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2021-12-03 08:51:51 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+# Install XFCE GUI, VNC server, and other necessary packages
+
+apk add --no-cache ca-certificates curl openssl sudo xvfb x11vnc xfce4 xfce4-terminal faenza-icon-theme bash procps nano git
+
+# Set VNC password: ("alpine" but you can set it to whatever)
+
+mkdir -p /root/.vnc && x11vnc -storepasswd alpine /root/.vnc/passwd
+
+# Start GUI and VNC server services
+
+export DISPLAY=:99
+export RESOLUTION=1920x1080x24
+
+nohup /usr/bin/Xvfb :99 -screen 0 $RESOLUTION -ac +extension GLX +render -noreset > /dev/null 2>&1 &
+
+nohup startxfce4 > /dev/null 2>&1 &
+
+nohup x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :99 -forever -bg -rfbauth /root/.vnc/passwd -users root -rfbport 5900 > /dev/null 2>&1 &
+
+# This network config file with a loopback interface is needed for libvirtd and noVNC (loopback) services to start successfully
+
+cat > /etc/network/interfaces <<EOF
+auto lo
+iface lo inet loopback
+EOF
+
+# Install KVM hypervisor
+
+apk add libvirt-daemon qemu-img qemu-system-x86_64 qemu-modules virt-manager
+rc-update add libvirtd
+modprobe tun
+modprobe br_netfilter
+grep -q -E 'vmx' /proc/cpuinfo && modprobe kvm-intel
+grep -q -E 'svm' /proc/cpuinfo && modprobe kvm-amd
+
+# Enable VFIO PCI Passthrough
+
+modprobe vfio_pci
+modprobe vfio_iommu_type1
+chown qemu /dev/vfio/vfio
+
+# Start libvirtd service
+
+rc-service libvirtd start
+
+# Install web-browser (Firefox works, Chromium seems to throw an I/O error and doesn't launch)
+
+apk add firefox-esr
+
+# Install NoVNC (VNC client over http)
+
+export NOVNC_TAG=$(curl -s https://api.github.com/repos/novnc/noVNC/releases/latest | jq -r .tag_name)
+
+export WEBSOCKIFY_TAG=$(curl -s https://api.github.com/repos/novnc/websockify/releases/latest | jq -r .tag_name)
+
+git clone --depth 1 https://github.com/novnc/noVNC --branch ${NOVNC_TAG} /root/noVNC
+
+git clone --depth 1 https://github.com/novnc/websockify --branch ${WEBSOCKIFY_TAG} /root/noVNC/utils/websockify
+
+cp /root/noVNC/vnc.html /root/noVNC/index.html
+
+sed -i "s/UI.initSetting('resize', 'off');/UI.initSetting('resize', 'scale');/" /root/noVNC/app/ui.js
+
+/root/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 80
