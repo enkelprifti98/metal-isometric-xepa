a70983e87be331434eb69c2e038f81aa8b4658cc
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2024-08-27 18:49:35 -0400

    Update setup-v2.sh

--- setup-v2.sh
+++ setup-v2.sh
@@ -447,6 +447,7 @@ VIRT_INSTALL_PARAMS='virt-install --name xepa --description "XEPA ISO Installer 
 # --os-variant detect=off \
 # --os-variant detect=on \
 # --virt-type kvm \
+# --machine q35  using q35 chipset instead of i440fx is required for PCIe support otherwise passing specific PCI IDs to the guest instead of getting random IDs like address.domain=0x$PCI_DOMAIN will not work
 
 if [ -d /sys/firmware/efi ]; then
     VIRT_INSTALL_PARAMS=$VIRT_INSTALL_PARAMS$'--boot uefi '
